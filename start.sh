5f5acb3fbd109c0587bd42902770b24f95109872
dfahlander <david.fahlander@gmail.com>
2019-09-03 22:11:24 +0200

    Add "offline" switch on landing page also

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-jekyll serve --incremental &
-echo $! > jekyll.pid
-echo "Surf to http://localhost:4000"
-echo "To stop jekyll and http-server, run ./stop.sh"
+bundle exec jekyll serve --incremental
+#echo $! > jekyll.pid
+#echo "Surf to http://localhost:4000"
+#echo "To stop jekyll and http-server, run ./stop.sh"

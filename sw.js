20c7fc7a38852f50c9c3f9d84753259ab2e43446
dfahlander <david.fahlander@gmail.com>
2019-09-04 11:11:49 +0200

    Optimizing for offline/online/slow network

--- sw.js
+++ sw.js
@@ -85,27 +85,60 @@ self.addEventListener('install', function (event) {
             do {
                 // cache in batches of 10
                 const sliceDice = leadingSlashRemoved.splice(0, 50);
-                await waitForAll(sliceDice.map(async url => cache.put(url, await fetch((url)))));
+                await waitForAll(sliceDice.map(async url => {
+                    const response = await fetch(url);
+                    //console.log("URL", url, response);
+                    await cache.put(url, response);
+                }));
             } while(leadingSlashRemoved.length > 0);
             return true;
         })
     );
 });
 
-async function respondFromCache(cache, origRes, req, origError) {
-    const cacheResult = await cache.match(req);
-    if (cacheResult) return cacheResult;
-    if (origError) throw origError;
-    return origRes;
-}
+const MAX_WAIT = 300; // If network responds slower than 100 ms, respond with cache instead
 
 self.addEventListener('fetch', function (event) {
-    event.respondWith(fetch(event.request).then(async res => {
-        const cache = await caches.open(CACHE_NAME);
-        if (!res.ok) return respondFromCache(cache, res, event.request);
-        cache.put(event.request, res.clone());
+    // Start reading from cache
+    const cachedResponsePromise = caches.match(event.request);
+    const fetchPromise = fetch(event.request);
+    const timeoutPromise = new Promise(resolve => {
+        setTimeout(()=>resolve("timedout"), MAX_WAIT);
+    });
+    // Start fetching in parallell
+    event.respondWith(Promise.race([fetchPromise, timeoutPromise]).then(async res => {
+        // Fetch successful, probably online. See if we also have the cached response:
+        const cachedResponse = await cachedResponsePromise.catch(err => null);
+        if (res === "timedout" || !res.ok) {
+            // Fetch didn't throw but the result wasn't ok either.
+            // Could be timeout, a 404, 500 or maybe offline?
+            // In case we have an OK response in the cache, respond with that one instead:
+            if (cachedResponse && cachedResponse.ok) {
+                if (res === "timedout") console.log("URL", event.request.url, "timedout. Serving it from cache to speed up site");
+                return cachedResponse;
+            } else if (res === "timedout") {
+                // We don't have anything in cache. Wait for fetch even if it takes time.
+                res = await fetchPromise;
+            } else {
+                return res;
+            }
+        }
+
+        if (res.ok) {
+            // Should we update the cache with this fresh version?
+            if (!cachedResponse || (cachedResponse.headers.get("last-modified") !== res.headers.get("last-modified"))) {
+                // There were no cached response, or "last-modified" headers was changed - keep the cache up-to-date,
+                // so that, when the user goes offline, it will have the latest and greatest, and not revert to old versions
+                const cache = await caches.open(CACHE_NAME);
+                await cache.put(event.request, res.clone());
+            }
+        }
         return res;
     }, async error => {
-        return respondFromCache(await caches.open(CACHE_NAME), null, event.request, error);
+        const cachedResponse = await cachedResponsePromise.catch(err => null);
+        if (cachedResponse && cachedResponse.ok) {
+            return cachedResponse;
+        }
+        throw error;
     }));
 });

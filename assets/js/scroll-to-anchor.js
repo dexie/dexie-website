2d9567a062a9cdc9715e3152742d2fe025aa40fd
dfahlander <david.fahlander@gmail.com>
2024-03-27 02:54:37 +0100

    Fix support links

--- assets/js/scroll-to-anchor.js
+++ assets/js/scroll-to-anchor.js
@@ -1,14 +1,21 @@
-document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-  anchor.addEventListener("click", function (e) {
-    var href = this.getAttribute("href");
-    if (href === "#") return;
-    var elem = document.querySelector(href);
-    if (elem) {
-      e.preventDefault();
-      elem.scrollIntoView({
-        behavior: "smooth",
-      });
-      history.pushState(null, null, location + href);
-    }
-  });
+document.querySelectorAll('a[href]').forEach((anchor) => {
+  var href = anchor.getAttribute("href");
+  if (href === "#") return;
+  if (href.startsWith(location.pathname + '#' || href.startsWith(location.pathname + '/#'))) {
+    href = href
+      .replace(location.pathname + '/', '')
+      .replace(location.pathname, '');
+  }
+  if (href.startsWith('#')) {
+    anchor.addEventListener("click", function (e) {
+      var elem = document.querySelector(href);
+      if (elem) {
+        e.preventDefault();
+        elem.scrollIntoView({
+          behavior: "smooth",
+        });
+        history.pushState(null, null, location.href.split('#')[0] + href);
+      }
+    });
+  }
 });

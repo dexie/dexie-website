054a4598b8f49b7d6c051e84f67e3e9eb10d797d
dfahlander <david.fahlander@gmail.com>
2024-04-30 13:44:06 +0200

    Hide offline functionality if no cookies are accepted

--- assets/js/offline-switch.js
+++ assets/js/offline-switch.js
@@ -1,53 +1,64 @@
-function setOffline (checked) {
+function setOffline(checked) {
   if (!('serviceWorker' in navigator)) {
     event.preventDefault();
     return;
   }
-  var offlineSwitch = document.getElementById("offline-switch");
-  offlineSwitch.className = checked ? "checked" : "";
+  var offlineSwitch = document.getElementById('offline-switch');
+  offlineSwitch.className = checked ? 'checked' : '';
 
   if (checked) {
-    localStorage.setItem("offline", "true");
-    navigator.serviceWorker.register('/sw.js').then(function (registration) {
-      console.log('Service Worker registration successful with scope: ', registration.scope);
-    }).catch(function (err) {
-      var checkbox = document.getElementById("offline-checkbox");
-      checkbox.checked = false;
-      offlineSwitch.className = "";
-	  offlineSwitch.setAttribute("aria-labelledby","offlineOffHeading");
-    });
-	
+    localStorage.setItem('offline', 'true');
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then(function (registration) {
+        console.log(
+          'Service Worker registration successful with scope: ',
+          registration.scope
+        );
+      })
+      .catch(function (err) {
+        var checkbox = document.getElementById('offline-checkbox');
+        checkbox.checked = false;
+        offlineSwitch.className = '';
+        offlineSwitch.setAttribute('aria-labelledby', 'offlineOffHeading');
+      });
   } else {
-    localStorage.setItem("offline", "false");
-    navigator.serviceWorker.getRegistration("/sw.js").then(function (reg) {
-      console.log("Unregistering...");
-      return reg.unregister();
-    }).then(function (res) {
-      console.log("Unregistered", res);
-    }).catch(function(error) {
-      console.error(error);
-    });
+    localStorage.setItem('offline', 'false');
+    navigator.serviceWorker
+      .getRegistration('/sw.js')
+      .then(function (reg) {
+        console.log('Unregistering...');
+        return reg.unregister();
+      })
+      .then(function (res) {
+        console.log('Unregistered', res);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
   }
 }
 
 function updateSwitchFromStorage() {
-  var offlineSwitch = document.getElementById("offline-switch");
-  var offline = localStorage.getItem("offline");
-  offlineSwitch.className = offline === "true" ? "checked" : "";
-  var checkbox = document.getElementById("offline-checkbox");
-  checkbox.checked = offline === "true" ? true : false;
-  if (!offline && ('serviceWorker' in navigator)) {
+  var offlineSwitch = document.getElementById('offline-switch');
+  var offlineSwitchEnabled = document.getElementById('offline-switch-enabled');
+  var cookieSettingsIcon = document.getElementById('cookie-settings-icon');
+  offlineSwitchEnabled.style.display = '';
+  cookieSettingsIcon.style.display = 'none';
+  var offline = localStorage.getItem('offline');
+  offlineSwitch.className = offline === 'true' ? 'checked' : '';
+  var checkbox = document.getElementById('offline-checkbox');
+  checkbox.checked = offline === 'true' ? true : false;
+  if (!offline && 'serviceWorker' in navigator) {
     // Update the switch if offline is not in localstorage but site is indeed offline
-    navigator.serviceWorker.getRegistration("/sw.js").then(function (reg) {
+    navigator.serviceWorker.getRegistration('/sw.js').then(function (reg) {
       if (reg) {
-        offlineSwitch.className = "checked";
-		offlineSwitch.setAttribute("aria-labelledby","offlineOnHeading");
-        var checkbox = document.getElementById("offline-checkbox");
+        offlineSwitch.className = 'checked';
+        offlineSwitch.setAttribute('aria-labelledby', 'offlineOnHeading');
+        var checkbox = document.getElementById('offline-checkbox');
         checkbox.checked = true;
-        localStorage.setItem("offline", "true");
-		
+        localStorage.setItem('offline', 'true');
       }
     });
   }
 }
-
